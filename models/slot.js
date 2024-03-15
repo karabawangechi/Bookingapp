fd6757bf5cdb1b4f2454326cdc5f4abf62ca8940
Paul Ogolla <paulotieno2@gmail.com>
2024-03-15 18:46:00 +0300

    update slots

--- models/slot.js
+++ models/slot.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose');
 const slotSchema = mongoose.Schema({
   day: { type: String},
   time: { type: String},
-  userID: { type: String},
-  businessID: { type: String}
-})
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  business: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Business',
+    },
+  })
 
 const Slot = mongoose.model('Slot',slotSchema);
 
