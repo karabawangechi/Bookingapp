42a2205be669095169eec7c191828a671cc9121b
Stan-breaks <stanleymwendwa03@gmail.com>
2024-03-20 20:09:33 +0300

    init

--- express.js
+++ express.js
@@ -1,21 +1,3 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  const whitelist = [
-    'http://localhost:3000'
-  ];
-  const { origin } = req.headers;
-  if (whitelist.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,PATCH,DELETE',);
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-  }
-  return next();
-});
 
 app.use("/ussd", require("./routes/ussd"));
 
