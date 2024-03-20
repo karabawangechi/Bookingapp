5af77499f87e83b68ff1497be876a80f3d435b91
Stan-breaks <stanleymwendwa03@gmail.com>
2024-03-20 20:06:43 +0300

    init

--- express.js
+++ express.js
@@ -6,21 +6,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   const whitelist = [
-    "http://localhost:3000"
+    'http://localhost:3000'
   ];
   const { origin } = req.headers;
   if (whitelist.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods","OPTIONS,GET,PUT,POST,PATCH,DELETE",);
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,PATCH,DELETE',);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
   }
   return next();
 });
 
 app.use("/ussd", require("./routes/ussd"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 module.exports = app;
